75b2b291d9d780bb6beebb9c09bc29dfca0488f3
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2019-03-21 19:24:49 -0600

    Archivo

---
+++ Archivo.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class Archivo{
+
+    //Metodo para leer archivos de texto
+    //Recibe como parametro la direccion del lugar del archivo
+    public void leerTxt(String direccion){
+
+        String texto = "";
+
+        try{
+            BufferedReader bf = new BufferedReader(new FileReader(direccion));
+            //Variable temporal para guardar el texto
+            String temp = "";
+            //Variable para leer un buffer
+            String bfRead;
+            //Realiza el ciclo mientras bfRead tenga datos
+            while((bfRead = bf.readLine()) != null){
+                //Guarda el texto del archivo
+                temp = temp + bfRead;
+            }
+            texto = temp;
+
+            while()
+        } catch (Exception e){
+            System.out.println("No se encontro el archivo en la direccion dada");
+        }
+        return texto;
+    }
+
+}
---
+++ MainDiccionario.java
@@ -0,0 +1,11 @@
+public class MainDiccionario{
+    public static void main(String[] args){
+
+        //Leer un archivo de texto
+        Archivo a = new Archivo();
+        String texto = a.leerTxt("");
+        //direccion del archivo
+
+
+    }
+}
